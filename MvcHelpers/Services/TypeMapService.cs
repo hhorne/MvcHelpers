66b509d80642e793b11a700d6b4b07e41ca599cc
Heath <heath@logic-bomb.com>
2012-07-07 22:52:57 -0400

    Created a type map service to wrap AutoMapper
    Type map service will wrap AutoMapper to make it easier to test
    components that may rely on type mapping services.

---
+++ MvcHelpers/Services/TypeMapService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+
+namespace MvcHelpers.Services
+{
+    public class TypeMapService : ITypeMapService
+    {
+        public void CreateMap(Type source, Type dest)
+        {
+            Mapper.CreateMap(source, dest);
+        }
+
+        public void CreateMap<TSource, TDest>()
+        {
+            Mapper.CreateMap<TSource, TDest>();
+        }
+
+        public bool TypeMapExists(Type source, Type dest)
+        {
+            return Mapper.FindTypeMapFor(source, dest) != null;
+        }
+
+        public bool TypeMapExists<TSource, TDest>()
+        {
+            return Mapper.FindTypeMapFor<TSource, TDest>() != null;
+        }
+
+        public void Reset()
+        {
+            Mapper.Reset();
+        }
+    }
+
+    public interface ITypeMapService
+    {
+        void CreateMap(Type source, Type dest);
+        void CreateMap<TSource, TDest>();
+        bool TypeMapExists(Type source, Type dest);
+        bool TypeMapExists<TSource, TDest>();
+        void Reset();
+    }
+}
