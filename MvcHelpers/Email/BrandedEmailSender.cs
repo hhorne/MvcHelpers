6dc048dbf201f115e801657cc215346982ac5b1b
Heath <heath@logic-bomb.com>
2013-08-12 16:20:54 -0400

    Removed horror
    This method was silently swallowing exceptions and using exceptions to
    determine a return result. This prevents code that calls this method
    from catching the exception and acting on it.

--- MvcHelpers/Email/BrandedEmailSender.cs
+++ MvcHelpers/Email/BrandedEmailSender.cs
@@ -14,14 +14,13 @@ namespace MvcHelpers.Email
 		string BrandedTemplateFolder { get; set; }
 		string BrandedMessageFolder { get; set; }
 
-		Task<bool> SendEmail(DeliveryInstructions instructions);
+		Task SendEmail(DeliveryInstructions instructions);
 	}
 
 	public class BrandedEmailSender : IBrandedEmailSender
 	{
 		private readonly IIOService ioService;
 		private readonly IRazorTemplateResolver templateResolver;
-		private readonly ILog log;
 		private readonly IMailer mailer;
 
 		public string EmailPath { get; set; }
@@ -30,7 +29,6 @@ namespace MvcHelpers.Email
 
 		public BrandedEmailSender(IIOService ioService, IRazorTemplateResolver templateResolver, IMailer mailer)
 		{
-			this.log = LogManager.GetLogger(GetType());
 			this.ioService = ioService;
 			this.templateResolver = templateResolver;
 			this.mailer = mailer;
@@ -40,25 +38,15 @@ namespace MvcHelpers.Email
 			BrandedMessageFolder = "Messages";
 		}
 
-		public async Task<bool> SendEmail(DeliveryInstructions instructions)
+		public async Task SendEmail(DeliveryInstructions instructions)
 		{
-			try
+			if (instructions.Enabled == false)
 			{
-				if (instructions.Enabled == false)
-				{
-					log.Error("Unable to send BrandedEmail from BrandedEmailSender. Branded BrandedEmail is disabled.");
-					return false;
-				}
+				return;
+			}
 
-				var email = BuildEmailMessage(instructions);
-				await mailer.SendEmail(email);
-				return true;
-			}
-			catch (Exception e)
-			{
-				log.Error("Error in SendEmail in BrandedEmailSender", e);
-				return false;
-			}
+			var email = BuildEmailMessage(instructions);
+			await mailer.SendEmail(email);
 		}
 
 		private MailMessage BuildEmailMessage(DeliveryInstructions instructions)
